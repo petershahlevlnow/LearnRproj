fd1d7d4c52c162ff1517685c718f9cc5783caae3
Peter Shah <peter.shah@levlnow.com>
2016-01-21 16:18:50 -0800

    local db connection

--- test.R
+++ test.R
@@ -2,3 +2,5 @@ theURL <- "http://www.jaredlander.com/data/Tomato%20First.csv"
 theURL
 tomato <- read.table(file = theURL, header = TRUE, sep = ",")
 head(tomato)
+db <- odbcConnect("QV Training")
+customerTable <- sqlQuery(db, "SELECT * FROM bank.customer", stringsAsFactors = FALSE)
