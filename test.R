88c0389a47eb830fd4d665670f7d1f0bbd7b722f
petershahlevlnow <peter.shah@levlnow.com>
2016-01-21 16:35:56 -0800

    PW Test

--- test.R
+++ test.R
@@ -4,3 +4,4 @@ tomato <- read.table(file = theURL, header = TRUE, sep = ",")
 head(tomato)
 db <- odbcConnect("QV Training")
 customerTable <- sqlQuery(db, "SELECT * FROM bank.customer", stringsAsFactors = FALSE)
+test
