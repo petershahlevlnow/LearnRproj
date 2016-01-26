bc770e1f98c6e49d6921632c16668ab6112a50b1
petershahlevlnow <peter.shah@levlnow.com>
2016-01-26 10:27:30 -0800

    SQL Queries

--- test.R
+++ test.R
@@ -4,3 +4,8 @@ tomato <- read.table(file = theURL, header = TRUE, sep = ",")
 head(tomato)
 db <- odbcConnect("QV Training")
 customerTable <- sqlQuery(db, "SELECT * FROM bank.customer", stringsAsFactors = FALSE)
+head(customerTable)
+nameTable <- sqlQuery(db, "SELECT fname, lname 
+                            FROM bank.customer 
+                            INNER JOIN bank.individual 
+                            ON bank.customer.cust_id = bank.individual.cust_id", stringsAsFactors = FALSE)
