93d4a7dce7723afc79f86bb03ec1807aeb5f7bfb
petershahlevlnow <peter.shah@levlnow.com>
2016-01-26 11:32:36 -0800

    Added rdata files and folder

--- test.R
+++ test.R
@@ -9,3 +9,19 @@ nameTable <- sqlQuery(db, "SELECT fname, lname
                             FROM bank.customer 
                             INNER JOIN bank.individual 
                             ON bank.customer.cust_id = bank.individual.cust_id", stringsAsFactors = FALSE)
+
+save(tomato, file= "data/tomato.rdata")
+rm(tomato)
+head(tomato)
+load("data/tomato.rdata")
+head(tomato)
+
+n <- 20
+r <- 1:10
+w <- data.frame(n,r)
+n
+r
+w
+save(n,r,w,file = "data/multiple.rdata")
+rm(r,n,w)
+load("data/multiple.rdata")
