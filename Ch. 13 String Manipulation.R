990a3ba182d1bc359e4a5b0fff33b04b1a7e3a4f
petershahlevlnow <pshah82@gmail.com>
2016-03-11 15:57:50 -0800

    13.1 paste

--- Ch. 13 String Manipulation.R
+++ Ch. 13 String Manipulation.R
@@ -1,1 +1,17 @@
 # Ch 13 manipulating strings
+
+# 13.1 paste
+# notice spaces automatically added between strings with no third arg defined
+paste("hello", "Jared", "and others")
+paste("hello", "Jared", "and others", sep = "/")
+# can aslo paste vectors, paired
+paste(c("hello", "jared", "winters"), c("meep", "zort", "narf"))
+paste("hello", c("jared", "winters", "meep"))
+paste("hello", c("jared", "winters", "meep"), "goodbye")
+
+# use paste to collapse a vector
+vectorOftext <- c("hello", "everyone", "out there", ".")
+paste(vectorOftext, collapse = " ")
+paste(vectorOftext, collapse = "*")
+
+
