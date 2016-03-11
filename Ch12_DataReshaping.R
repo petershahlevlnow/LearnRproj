3a840f7dcc9bc91b1dbf690253677e67aca7d6cf
petershahlevlnow <pshah82@gmail.com>
2016-03-11 11:50:26 -0800

    Download and unzip foreign aid files

--- Ch12_DataReshaping.R
+++ Ch12_DataReshaping.R
@@ -1,6 +1,6 @@
 #Chapter 12 - data reshaping
 
-# cbind and rbind - combine two or more datasets of identical rows and columns
+# 12.1 cbind and rbind - combine two or more datasets of identical rows and columns
 # make two vectors and combine them as columns in data.frame
 sport <- c("hockey", "baseball", "basketball", "football")
 leauge <- c("NHL", "MLB", "NBA", "NFL")
@@ -13,3 +13,9 @@ trophies2 <- data.frame(sport = c("basketball", "golf"), leauge = c("DNBA", "PGA
 trophies <- rbind(trophies1, trophies2)
 # can also change column titles with cbind
 cbind(Sport = sport, Association = leauge, Prize = trophy)
+
+# 12.2 Joins
+# first download and unzip file for url
+download.file(url = "http://jaredlander.com/data/US_Foreign_Aid.zip", 
+              destfile = "data/ForeignAid.zip")
+unzip("data/ForeignAid.zip", exdir = "data")
