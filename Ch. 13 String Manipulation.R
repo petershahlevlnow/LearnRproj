fc32d458eeadc12fd9b18c9808f9aa0658db512b
petershahlevlnow <pshah82@gmail.com>
2016-03-11 16:07:05 -0800

    sprintf

--- Ch. 13 String Manipulation.R
+++ Ch. 13 String Manipulation.R
@@ -16,3 +16,11 @@ paste(vectorOftext, collapse = "*")
 
 #13.2 sprintf
 
+person <- "Jared"
+partysize <- "eight"
+waitTime <- 25
+paste("Hello ", person, ", your party of ", partysize, " will be seated in ", waitTime, " minutes.",sep = "")
+sprintf("Hello %s, your party of %s will be seated in %s minutes.", person, partysize, waitTime)
+#sprintf can also be vectorized, but must be multiples of each other
+sprintf("Hello %s, your party of %s will be seated in %s minutes.", c("Jared", "Bob"), c("eight", 16, "four", 10),
+        waitTime)
