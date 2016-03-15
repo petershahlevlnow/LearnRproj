f080365156fb236144661b0d45f8dd19913c1576
petershahlevlnow <pshah82@gmail.com>
2016-03-14 20:42:13 -0700

    binomial distribution - rbinom, pbinom, qbinom

--- Ch 14 Prob Distributions.R
+++ Ch 14 Prob Distributions.R
@@ -60,3 +60,55 @@ qnorm(0.5)
 randNorm10
 qnorm(pnorm(randNorm10))
 all.equal(randNorm10, qnorm(pnorm(randNorm10)))
+
+#14.2 binomial distribution
+# number af successes of independent trials using rbinom, 
+# n = number of runs, size = trial size, prob = success
+rbinom(n = 1, size = 10, prob = 0.4)
+rbinom(n = 5, size = 10, prob = 0.4)
+rbinom(n = 10, size = 10, prob = 0.4)
+
+# setting size to one reduces to Bernoulli distribution
+rbinom(n = 1, size = 1, prob = 0.4)
+rbinom(n = 5, size = 1, prob = 0.4)
+rbinom(n = 10, size = 1, prob = 0.4)
+
+
+#visual illustration
+binomData <- data.frame(Successes = rbinom(n = 10000, size = 10, prob = 0.3))
+ggplot(binomData, aes(x = Successes)) + geom_histogram(binwidth = 1)
+
+#see how as trial size increases it begins to approximate a normal distribution
+bnom5 <- data.frame(Successes = rbinom(n = 10000, size = 5, prob = 0.3), Size = 5)
+dim(bnom5)
+head(bnom5)
+# increase trial size to 10, 100, 10000
+bnom10 <- data.frame(Successes = rbinom(n = 10000, size = 10, prob = 0.3), Size = 10)
+dim(bnom10)
+head(bnom10)
+bnom100 <- data.frame(Successes = rbinom(n = 10000, size = 100, prob = 0.3), Size = 100)
+dim(bnom100)
+head(bnom100)
+bnom10000 <- data.frame(Successes = rbinom(n = 10000, size = 10000, prob = 0.3), Size = 10000)
+dim(bnom10000)
+head(bnom10000)
+# bind all rows
+binomAll <- rbind(bnom5, bnom10, bnom100, bnom10000)
+dim(binomAll)
+head(binomAll, 10)
+tail(binomAll, 10)
+# plot with facets
+ggplot(binomAll, aes(x=Successes)) + geom_histogram() + facet_wrap(~ Size, scales = "free")
+
+# dbinom (probability of exact value), pbinom (cumalitive probabilty)
+# probability of 3 successes out of 10
+dbinom(x = 3, size = 10, prob = 0.3)
+# probability of 3 or fewer successes out of 10
+pbinom(q = 3, size = 10, prob = 0.3)
+# both functions can be vectorized
+dbinom(x = 1:10, size = 10, prob = 0.3)
+pbinom(q = 1:10, size = 10, prob = 0.3)
+
+#qbinom - given probabilty, returns quantile - for this distrubution the number of successes
+qbinom(p = 0.3, size = 10, prob = 0.3)
+qbinom(p = c(0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6), size = 10, prob = 0.3)
