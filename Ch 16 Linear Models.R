acf7e354f35cf7a16d4693c85af29400189b8cb7
petershahlevlnow <pshah82@gmail.com>
2016-03-16 16:20:45 -0700

    Simple linear regression

--- Ch 16 Linear Models.R
+++ Ch 16 Linear Models.R
@@ -1,3 +1,61 @@
 #Ch 16 Linear Models
 
 #16.1 Simple Linear Regression
+
+require(UsingR)
+require(ggplot2)
+head(father.son)
+#plot fathers height to sons height. son's height is the response and father's height is the predictor
+ggplot(father.son, aes(x = fheight, y = sheight)) + geom_point() + geom_smooth(method = "lm") +
+  labs(x = "Fathers", y = "Sons")
+# lm outputs the intercept and coefficents where sheight is the response and fheight is the predictor
+heightsLM <- lm(sheight ~ fheight, data = father.son)
+heightsLM
+# summary prints all desired information about the LM
+summary(heightsLM)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
