8b39d23052d6c52ed6eee3bcec0fa791d89750b0
petershahlevlnow <pshah82@gmail.com>
2016-03-16 16:55:28 -0700

    16.1.1 ANOVA alternative

--- Ch 16 Linear Models.R
+++ Ch 16 Linear Models.R
@@ -14,14 +14,34 @@ heightsLM
 # summary prints all desired information about the LM
 summary(heightsLM)
 
+#16.1.1 ANOVA Alternative
+data(tips, package = "reshape2")
+head(tips)
+# adding a -1 indicates to leave out the intercept for the analysis/model
+tipsAnova <- aov(tip ~ day -1, data = tips)
+tipsLM <- lm(tip ~ day - 1, data =  tips)
+summary(tipsAnova)
+summary(tipsLM) # F statistic, df and p value are the same as ANOVA
 
+# calculate means and CI manually
+require(plyr)
+tipsByDay <- ddply(tips, "day", plyr::summarize, tip.mean= mean(tip), tip.sd = sd(tip), Length = NROW(tip),
+                   tfrac = qt(p=0.90, df = Length -1), Lower = tip.mean - tfrac*tip.sd/sqrt(Length),
+                   Upper = tip.mean + tfrac*tip.sd/sqrt(Length))
+# extract from the summary of tipsLM
+tipsInfo <- summary(tipsLM)
+tipsCoef <- as.data.frame(tipsInfo$coefficients[, 1:2])
+tipsCoef <- within(tipsCoef, {Lower <- Estimate - qt(p = 0.9, df = tipsInfo$df[2])*`Std. Error` 
+                              Upper <- Estimate + qt(p = 0.9, df = tipsInfo$df[2])*`Std. Error` 
+                                day <- row.names(tipsCoef)})
+#plot both
+ggplot(tipsByDay, aes(x = tip.mean, y = day)) + geom_point() +geom_errorbarh(aes(xmin = Lower,
+                                                                                 xmax = Upper), height = 0.3) +
+  ggtitle("tips by day calculated manually")
 
-
-
-
-
-
-
+ggplot(tipsCoef, aes(x = Estimate, y = day)) + geom_point() +geom_errorbarh(aes(xmin = Lower,
+                                                                                 xmax = Upper), height = 0.3) +
+  ggtitle("tips by day calculated from regression model")
 
 
 
