b8c304e1cb2b4c0b53b476d33edc671185b96fcf
petershahlevlnow <pshah82@gmail.com>
2016-03-18 11:48:10 -0700

    18.2 comparing models with anova, AIC, and BIC

--- Ch 18 Model Diagnostics.R
+++ Ch 18 Model Diagnostics.R
@@ -50,3 +50,39 @@ ggplot(house1, aes(x = .resid)) + geom_histogram()
 # correct specification
 
 # 18.2 Comparing Models
+# you only know how good your model is when comparing it to other models
+# fit multiple models
+house2 <- lm(housing$ValuePerSqFt ~ Units * SqFt + Boro, data = housing)
+house3 <- lm(housing$ValuePerSqFt ~ Units + SqFt * Boro + Class, data = housing)
+house4 <- lm(housing$ValuePerSqFt ~ Units + SqFt * Boro + SqFt * Class, data = housing)
+house5 <- lm(housing$ValuePerSqFt ~ Boro + Class, data = housing)
+
+# visualize the coefficients
+multiplot(house1, house2, house3, house4, house5, pointSize = 2)
+# shows that Boro and some condo classes matter
+
+# ANOVA is useful to test multiple models, it returns the residual sum of squares (RSS). The lower the better
+anova(house1, house2, house3, house4, house5)
+# a problem with the RSS is that when adding a variable to the model it improves the RSS, which can lead to 
+# overfitting and complexity
+# The AIC penalizes for over complexity. Lower the better
+AIC(house1, house2, house3, house4, house5)
+# the BIC is the same, except a difference in the BIC formula
+BIC(house1, house2, house3, house4, house5)
+
+# when the anova, AIC, and BIC are called on glm the deviance is returned. For every added variable the deviance
+# should decrease by 2
+# try it now with a few logistic models
+# create a binary variable based on whether value per square foot is greater than 150
+housing$highValue <- housing$ValuePerSqFt >= 150
+
+high1 <- glm(housing$highValue ~ Units * SqFt + Boro, data = housing, family = binomial(link = "logit"))
+high2 <- glm(housing$highValue ~ Units * SqFt + Boro, data = housing, family = binomial(link = "logit"))
+high3 <- glm(housing$highValue ~ Units + SqFt * Boro + Class, data = housing, family = binomial(link = "logit"))
+high4 <- glm(housing$highValue ~ Units + SqFt * Boro + SqFt * Class, data = housing, family = binomial(link = "logit"))
+high5 <- glm(housing$highValue ~ Boro + Class, data = housing, family = binomial(link = "logit"))
+
+anova(high1, high2, high3, high4, high5)
+AIC(high1, high2, high3, high4, high5)
+BIC(high1, high2, high3, high4, high5)
+
