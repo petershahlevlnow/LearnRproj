aa65089a804ff09c4e8139927f74ae5da8e55520
petershahlevlnow <pshah82@gmail.com>
2016-03-17 17:14:07 -0700

    17.2 poission regression

--- Ch 17 GLM.R
+++ Ch 17 GLM.R
@@ -26,5 +26,36 @@ invlogit <- function(x)
 
 invlogit(income1$coefficients)
 
+#17.2 poisson regression
+# used on count data
+# investigate num of children
+#first plot the data to investigate, not perfectly poisson but still can fit a possion model to it
+ggplot(acs, aes(x = NumChildren)) + geom_histogram(binwidth = 1)
+# now fit a poisson model to NumChildren
+children1 <- glm(NumChildren ~ FamilyIncome + FamilyType + OwnRent, data = acs, family = poisson(link = "log"))
+summary(children1)
+coefplot(children1)
+# a concern for poission regression is overdispersion - variability in the data is greater than theorized by
+# a poission distribution where mean and variance are the same
+# calculate the overdispersion
+# standardized residuals
+z <- (acs$NumChildren - children1$fitted.values)/sqrt(children1$fitted.values)
+# over dispersion factor
+sum(z^2)/children1$df.residual
+# overdispersion p-value
+pchisq(sum(z^2), children1$df.residual)
+# in general a overdispersion factor greater than 2 indicates overdispersion, while this ratio is less than 2 the
+# p-vale is 1 meaning there is statistically significant overdispersion. So we need to refit the model
+# to account for overdispersion using the quassipoisson
+children2 <- glm(NumChildren ~ FamilyIncome + FamilyType + OwnRent, data = acs, family = quasipoisson(link = "log"))
+summary(children2)
+multiplot(children1, children2)
+#because the overdispersion was not that large there is a little more uncertaintity 
 
+#17.3 other glm 
+# glm function supports - gamma, inverse gaussian, quasibinomial, different link functions can be supplied - 
+# logit, probit, cauchit, log and cloglog for binomial; inverse, identity and log for gamma; log, idendtity
+# and sqrt poision; and 1/mu^2, inverse, identity and log for inverse gaussian
 
+# Multinomial regression - classifying multiple categories: 1. running multiple logistic regressions. 2. using
+# polr function 3. multinom function from the nnet package
