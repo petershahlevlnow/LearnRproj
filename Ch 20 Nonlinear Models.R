92f5c5e19e804ba0e0e3c928aa263ec9edef8529
petershahlevlnow <pshah82@gmail.com>
2016-03-23 17:39:33 -0700

    natural cubic splines - ns

--- Ch 20 Nonlinear Models.R
+++ Ch 20 Nonlinear Models.R
@@ -74,9 +74,15 @@ g + geom_line(data = splineDF, aes(x=x, y=y, color=factor(round(df, 0)), group=d
 # cubic splines - ns function
 require(splines)
 head(ns(diamonds$carat, df = 1))
+head(ns(diamonds$carat, df = 2))
+head(ns(diamonds$carat, df = 3))
+head(ns(diamonds$carat, df = 4))
+#new predictors can be used in any model just like other predictors more knots means a more intepolating fit
+# can plot these ggplot2 
 
-
-
+g <- ggplot(diamonds, aes(x=carat, y=price)) + geom_point()
+g + stat_smooth(method = "lm", formula = y ~ ns(x, 6), color = "blue") +
+  stat_smooth(method = "lm", formula = y ~ ns(x, 3), color = "red")
 
 
 
